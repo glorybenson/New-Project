e14b91d433cf317e2b3aa7abfaf34a15c248d85d
glorybenson <glorybson3738@gmail.com>
2024-06-15 06:05:06 +0100

    file update for two variables

--- name.php
+++ name.php
@@ -1,4 +1,7 @@
 <?php
-$name = $_GET['name'];
-echo 'Welcome to our website, ' . $name . '!';
+$firstname = $_GET['firstname'];
+$lastname = $_GET['lastname'];
+echo 'Welcome to our website, ' .
+ htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' . 
+ htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
 ?>
