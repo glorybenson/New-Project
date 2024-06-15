701614a8e903e7e0de8afc1f75b049d89d459540
glorybenson <glorybson3738@gmail.com>
2024-06-15 06:57:06 +0100

    PHP-Form-Post request

--- name.php
+++ name.php
@@ -1,7 +1,7 @@
 <?php
-$firstname = $_GET['firstname'];
-$lastname = $_GET['lastname'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
 echo 'Welcome to our website, ' .
- htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' . 
+ htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' .
  htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
 ?>
